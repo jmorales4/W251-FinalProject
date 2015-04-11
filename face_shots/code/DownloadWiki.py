c4bf9d6c9b658e294641db5b5e744c5302ced7ac
Joe Morales <joseph.morales@flukenetworks.com>
2015-04-11 16:55:50 -0600

    Move resulting wiki out of code project and run with 30 threads

--- face_shots/code/DownloadWiki.py
+++ face_shots/code/DownloadWiki.py
@@ -2,6 +2,8 @@ import os
 import urllib2
 import sys
 import thread
+from concurrent.futures import ThreadPoolExecutor
+import time
 
 __author__ = 'jmorales'
 
@@ -9,7 +11,6 @@ __author__ = 'jmorales'
 def download(path, url):
     with open(path, 'w') as f:
         try:
-            print path
             f.write(urllib2.urlopen(url).read())
         except Exception as e:
             print 'Could not download {}: {}'.format(url, e)
@@ -17,14 +18,15 @@ def download(path, url):
 
 if __name__ == "__main__":
     urls = {}
-    with open('names.txt') as f:
+    with open('../names.mat') as f:
         urls = dict(map(lambda name: (name.strip(), 'http://en.wikipedia.org/wiki/' + name.replace(' ', '_')), f))
 
-    for name in urls:
-        file_path = 'wiki2\\' + name
-        if os.path.isfile(file_path): continue
 
-        download(file_path, urls[name])
+    with ThreadPoolExecutor(max_workers=30) as executor:
+        for name in urls:
+            file_path = '../wiki/' + name
+            if os.path.isfile(file_path): continue
+            executor.submit(download, file_path, urls[name]);
 
 
 
