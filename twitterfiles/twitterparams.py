e495020928a46fcadd08042ba8f8e333ec27b392
dmilad <dmilad@gmail.com>
2015-04-23 21:14:40 -0700

    remove twitter app params

--- twitterfiles/twitterparams.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'dmilad'
-
-OAuthConsKey = 'o9P560AKPUcACYY1hPFmfQUVX'
-OAuthConsSecret = 'xmF127RAYgH7qKHnWJkKtchU7ocSLq12sh8CLys3tQjzbAprw3'
-OAuthToken = '18185125-cU0fb3TdTR2pVIZHOqkHkZzaywt6ORp5JNEyBPDV3'
-OAuthTokenSecret = 'IbVSOJaHDuX108FawfDSNgdjqfijA4eCny72k2KRy5O0c'
--- twitterfiles/twitterparams_1.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'dmilad'
-
-OAuthConsKey = 'mDZKH27RCgUPefhY9pDTug'
-OAuthConsSecret = 'OH7GDf4cZwxneVsQP4sqJcLzAlOvxg3fBQXt4VArw'
-OAuthToken = '18185125-KhTdy39L4aPBEDqkpVr7jse11xDILS8nRDHFtVv7d'
-OAuthTokenSecret = '5yCSTsEZLlywmLrxwKW7bfaaBBOIZm0MFhVrAjSmgv62D'
