272d95dafdf8f4abce2d1b0903e286cc4e4e7d1e
Joe Morales <joseph.morales@flukenetworks.com>
2015-04-27 08:54:37 -0600

    Catch tweets not found error

--- face_shots/code/tweets.py
+++ face_shots/code/tweets.py
@@ -15,22 +15,27 @@ class page:
         person = input.person if input.has_key('person') else ''
 
         # Get the latest tweets
-        tweets = self.get_tweets(person)
+        try:
+            tweets = self.get_tweets(person)
 
-        tweet_html = ''
-        for tweet in reversed(tweets):
-            tweet_html = tweet_html + str(render_tweet( tweet['name'], tweet['screen_name'], tweet['profile_image_url'], tweet['created_at'], tweet['text']))
+            tweet_html = ''
+            for tweet in reversed(tweets):
+                tweet_text = tweet['text'].decode('raw-unicode-escape').encode('utf-8')
+                tweet_html += str(render_tweet(tweet['name'], tweet['screen_name'], tweet['profile_image_url'], tweet['created_at'], tweet_text))
 
-            # And render the results via templates/tweets.html
-        return render_tweets(person, tweet_html)
+                # And render the results via templates/tweets.html
+            return render_tweets(person, tweet_html)
+
+        except Exception as e:
+            return render_tweets(person, str(e))
 
     def get_tweets(self, person):
         user = os.environ.get('OBJECT_STORE_USER')
-        api_key =  os.environ.get('OBJECT_STORE_API_KEY')
-        datacenter =  os.environ.get('OBJECT_STORE_DATACENTER')
+        api_key = os.environ.get('OBJECT_STORE_API_KEY')
+        datacenter = os.environ.get('OBJECT_STORE_DATACENTER')
         sl_storage = object_storage.get_client(user, api_key, datacenter=datacenter)
-        return json.loads(('[' + sl_storage['faces_tweets_latest'][person.lower() + '.tweets.json.txt'].read() + ']')\
-            .replace('}', '},').replace('},]', '}]'))
+        return json.loads(('[' + sl_storage['faces_tweets_latest'][person.lower() + '.tweets.json.txt'].read() + ']') \
+                          .replace('}', '},').replace('},]', '}]'))
 
 
 
